4ac10022d6fddc1add18755c70910df848eeff27
szkyudi <iamsucchii@gmail.com>
2022-08-23 22:01:47 +0900

    文言の修正

--- components/organisms/CurrentStation.tsx
+++ components/organisms/CurrentStation.tsx
@@ -10,7 +10,7 @@ export const CurrentStation: React.FC<Props> = ({ name, list }) => {
       <h1>{name}</h1>
       <div>
         <h2>{name}駅から{list.length - 1}駅以内の駅の一覧</h2>
-        <h3>溜池山王駅が通っている路線の一覧</h3>
+        <h3>{name}駅が通っている路線の一覧</h3>
         <ul>
           {list.map((station, index) => {
             return (
