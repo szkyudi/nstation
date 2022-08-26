57dc32a4b4b2858205207bf29213a08e741e1c1a
szkyudi <iamsucchii@gmail.com>
2022-08-26 09:47:58 +0900

    debug用ページの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
+import { getStationsByName } from '@/lib/api/getStationsByName'
 import type { NextPage } from 'next'
+import { FormEventHandler, useState } from 'react';
 
 const Home: NextPage = () => {
+  const [name, setName] = useState('');
+  const handleSubmit: FormEventHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const station = await getStationsByName(name)
+      console.log(station);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
     <div>
-
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={(e) => setName(e.currentTarget.value)} />
+        <button>送信</button>
+      </form>
     </div>
   )
 }
