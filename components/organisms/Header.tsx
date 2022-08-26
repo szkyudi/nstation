65b25b8731728a290f208dda51800f529a915afe
szkyudi <iamsucchii@gmail.com>
2022-08-26 19:05:30 +0900

    API Routeの利用

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -2,14 +2,81 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import SearchIcon from '@mui/icons-material/Search';
+import { alpha, InputBase, styled } from '@mui/material';
+import { useRouter } from 'next/router';
+
+
+const Search = styled('form')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 export const Header = () => {
+  const [name, setName] = React.useState("");
+  const router = useRouter();
+
+  const handleSubmit: React.FormEventHandler = (e) => {
+    e.preventDefault();
+    if (name) {
+      router.push(`/stations/${name}`);
+    }
+  }
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography component="h1" variant="h6" sx={{ flexGrow: 1 }}>
-          N駅以内の駅探し
+        <Typography component="h1" variant="h6" sx={{ flexGrow: 1, mr: 2 }}>
+          nStation
         </Typography>
+        <Search onSubmit={handleSubmit}>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="駅検索"
+            inputProps={{ 'aria-label': 'search' }}
+            value={name}
+            onChange={(e) => setName(e.currentTarget.value)}
+          />
+        </Search>
       </Toolbar>
     </AppBar>
   );
--- lib/api/getStations.ts
+++ lib/api/getStations.ts
@@ -1,9 +1,9 @@
-import { API_URL } from "@/constants";
+import { APP_URL } from "@/constants";
 import { GetStationRequest, GetStationResponse } from "@/interfaces/api/getStations";
 import { Station } from "@/interfaces/station";
 
 export async function getStations(params: GetStationRequest): Promise<Station[]> {
-  const url = new URL(API_URL);
+  const url = new URL(`${APP_URL}/api/stations`);
   for (const [key, value] of Object.entries(params)) {
     value && url.searchParams.set(key, value);
   }
