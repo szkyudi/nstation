5d82913f9644bbccb4f18090256ef431b987467b
szkyudi <iamsucchii@gmail.com>
2022-09-04 16:51:49 +0900

    CORSの設定

--- pages/api/stations.ts
+++ pages/api/stations.ts
@@ -1,7 +1,29 @@
 import { EXPRESS_API_URL } from "@/constants"
 import { NextApiRequest, NextApiResponse } from "next";
+import Cors from 'cors';
+
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD']
+});
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await runMiddleware(req, res, cors);
   const url = new URL(EXPRESS_API_URL);
   for (const key in req.query) {
     const value = req.query[key];
