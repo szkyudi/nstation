1cfb3991aad1ff5ed732aef697efe023cf25b3f4
szkyudi <iamsucchii@gmail.com>
2022-09-04 16:22:10 +0900

    URLの修正

--- constants.ts
+++ constants.ts
@@ -1,2 +1,3 @@
 export const EXPRESS_API_URL = 'https://express.heartrails.com/api/json';
-export const MY_APP_URL = process.env['NEXT_PUBLIC_VERCEL_URL'] || 'http://localhost:3000';
+export const VERCEL_URL = process.env['NEXT_PUBLIC_VERCEL_URL'];
+export const MY_APP_URL = VERCEL_URL ? `https://${VERCEL_URL}` : 'http://localhost:3000';
