1cfc0774d4936ccda68327f6e52fc8b23df6ef9c
szkyudi <iamsucchii@gmail.com>
2022-09-05 22:14:27 +0900

    アクセシビリティの向上

--- components/organisms/PopularStationChips.tsx
+++ components/organisms/PopularStationChips.tsx
@@ -1,5 +1,4 @@
-import { theme } from "@/lib/styles/theme";
-import { Avatar, Chip, Grid, useMediaQuery } from "@mui/material";
+import { Avatar, Chip, Grid } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import Link from "next/link";
 
@@ -7,8 +6,6 @@ type Props = {
   stations: string[];
 }
 export const PopularStationChips: React.FC<Props> = ({ stations }) => {
-  const isUpperSm = useMediaQuery(theme.breakpoints.up('sm'));
-
   return (
     <Grid container spacing={0.5}>
       {stations.map((name, index) => (
@@ -18,7 +15,6 @@ export const PopularStationChips: React.FC<Props> = ({ stations }) => {
               clickable
               component="a"
               variant="outlined"
-              size={isUpperSm ? 'medium' : 'small'}
               avatar={<Avatar sx={{ background: grey[300]}}>{index + 1}</Avatar>}
               label={name}
             />
